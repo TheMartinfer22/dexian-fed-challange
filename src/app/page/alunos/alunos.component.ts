55fee243a66d6dfd7d0bf2d6311a26b85eaa33ed
Martin Hartfelder <contato@nanosync.dev>
2024-05-08 17:32:57 -0300

    fix: removed debug

--- src/app/page/alunos/alunos.component.ts
+++ src/app/page/alunos/alunos.component.ts
@@ -132,7 +132,6 @@ export class AlunosComponent implements OnInit {
   }
 
   salvarAluno(): void {
-    console.log(this.novoAluno.dataNascimento)
     if (this.novoAluno.iCodAluno !== 0 && this.novoAluno.iCodAluno) {
       this.alunosService.updateAluno(this.novoAluno.iCodAluno, this.novoAluno).subscribe(() => {
         this.getAlunos();
